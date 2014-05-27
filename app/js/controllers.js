e8b7673aa73933e2979d525c91bdc270164cfd50
Piotr Kotłowski <piotr.kotlowski@gmail.com>
2014-05-27 17:40:56 +0200

    Code refactoring

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,34 +6,33 @@ var applicationControllers = angular.module('applicationControllers', []);
 var a;
 var result = new Array();
 
-applicationControllers.controller('DeleteController', ['$scope', '$rootScope',
-    '$route', '$routeParams', '$location',
-    function ($rootScope, $route, $routeParams, $location) {
+applicationControllers.controller('LoadDbController', ['$http', '$rootScope', '$location',
+    function ($http, $rootScope, $location) {
+        $http.get('js/videos.json').success(function (data) {
+            for (var i = 0; i < data.length; i++) {
+                var x = new Object(data[i]);
+                result.push(x);
+            }
+        });
+        $rootScope.a = result;
+        $location.path("/videos");
+    }]);
+applicationControllers.controller('AllVideosController', function ($route, $routeParams, $location, $scope, $rootScope) {
+
+    $scope.videos = $rootScope.a;
+    $scope.orderProp = 'gd$rating.average';
+
+    $scope.deleteVideo = function (id) {        
         for (var i = 0; i < result.length; i++) {
-            if ($routeParams.current.params.vidId === result[i].entry.id.$t.slice(42, 53)) {
+            if (id === result[i].entry.id.$t.slice(42, 53)) {
                 result.splice(i, 1);
                 break;
             }
         }
         $rootScope.a = result;
         $location.path("#/videos");
-    }]);
-applicationControllers.controller('LoadDbController', ['$http', '$rootScope', '$location',
-    function ($http, $rootScope, $location) {
-        $http.get('js/videos.json').success(function (data) {
-            for (var i = 0; i < data.length; i++) {
-                var x = new Object(data[i])
-                result.push(x)
-            }
-        });
-        $rootScope.a = result;
-        $location.path("/videos")
-    }]);
-applicationControllers.controller('AllVideosController', ['$scope', 'Vid', '$rootScope',
-    function ($scope, Vid, $rootScope) {
-        $scope.videos = $rootScope.a;
-        $scope.orderProp = 'gd$rating.average';
-    }]);
+    };
+});
 
 youTubeApp.directive('myYoutube', function ($sce) {
     return {
@@ -63,51 +62,53 @@ applicationControllers.controller('ShowVideoController', ['$scope', '$routeParam
                 break;
             }
         }
-//        $http.get('videos/' + $routeParams.videoId + '.json').success(function (data) {
-//            $scope.video = data;
-//        });
     }]);
-applicationControllers.controller('addNewVideo', function ($scope, $rootScope) {
+applicationControllers.controller('addNewVideo', function ($scope, $rootScope, $location) {
     $scope.addVideo = function () {
         var link = $scope.videoLink.toString();
         var id = "";
         if (link.search("youtube.com") > -1) {
             id = link.slice(32, 53);
-            $.getJSON("https://gdata.youtube.com/feeds/api/videos/" + id + "?&prettyprint=true&alt=json", function (data) {
-                result.push(new Object(data))
-                console.log(new Object(data))
-//                console.log(JSON.stringify(data, undefined, 2))
-            })
+
+            $.ajax({
+            url: "https://gdata.youtube.com/feeds/api/videos/" + id + "?&prettyprint=true&alt=json",
+            dataType: 'json',
+            async: false,            
+            success: function (data) {
+                $rootScope.a.push(new Object(data));
+                $location.path("#/videos")
+            }
+        });
         }
     };
-    $rootScope.a = result;
+     $rootScope.a= result;
+     //$location.path("#/videos")
 });
 
 applicationControllers.controller('searchInYtController', function ($scope, $rootScope, $location) {
     var tmp = new Array();
-    $scope.searchVideo = function () {
-        console.log("szukam")
+    $scope.searchVideo = function () {        
         $.ajax({
             url: "https://gdata.youtube.com/feeds/api/videos?q=" + $scope.searchedVideoTitle + "&max-result=3&prettyprint=true&alt=json",
             dataType: 'json',
-            async: false,
-            //data: data,
+            async: false,            
             success: function (data) {
-                console.log(data.feed.entry[0])
                 tmp.push(new Object(data.feed.entry[0]));
             }
         });
     };
     $scope.add = function (li) {
-        $.getJSON("https://gdata.youtube.com/feeds/api/videos/" + li + "?&prettyprint=true&alt=json", function (data) {
-            result.push(new Object(data));            
+        $.ajax({
+            url: "https://gdata.youtube.com/feeds/api/videos/" + li + "?&prettyprint=true&alt=json",
+            dataType: 'json',
+            async: false,            
+            success: function (data) {
+                result.push(new Object(data));
+            }
         });
         $rootScope.a = result;
         $location.path("#/videos");
     };
     $scope.videos = tmp;
 });
-//applicationControllers.controller('search', ['$scope',
-//    function ($scope) {
-//        $scope.videos = result;
-//    }]);
+
